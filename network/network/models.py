b1106335838a860179fcdb51737e02aaef6d9247
DrMostafaHammad <dr.mostafa.hammad84@gmail.com>
2024-03-26 12:09:29 +0300

    made models, check??

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,30 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Post(models.Model):
+    creator = models.ForeignKey("User", on_delete=models.CASCADE)
+    postcontent = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    likescount = models.IntegerField(default=0)
+    liked = models.BooleanField(default=False)
+
+
+class Followers(models.Model):
+    userbeingfollowed = models.ForeignKey("User", on_delete=models.CASCADE, related_name="userbfollowed")
+    followers = models.ManyToManyField("User", related_name="followers")
+
+
+class Following(models.Model):
+    userfollowing = models.ForeignKey("User", on_delete=models.CASCADE, related_name="usrfollowing")
+    followeesbeingfollowed = models.ManyToManyField("User", related_name="followees")
+
+
+
+# class Likes(models.Model):
+#     liker = models.ForeignKey("User", on_delete=models.CASCADE)
+#     post = models.ForeignKey("Post", on_delete=models.CASCADE)
+#     liked = models.BooleanField(default=False)
+    
+
